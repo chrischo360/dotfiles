15dcc2c44cb1dcfab766e1378446797c269cc085
Chris Cho <chrischo360@gmail.com>
2025-12-22 14:13:27 -0500

    remove lazy load startups

--- zsh/custom/03-lazy-load.zsh
+++ zsh/custom/03-lazy-load.zsh
@@ -12,66 +12,6 @@ gcloud() {
   command gcloud "$@"
 }
 
-# NOTE: pyenv, rbenv, fnm, SDKMAN have been replaced by mise
-# mise handles Node.js, Python, Ruby, Java version management
-# Configuration: ~/dotfiles/.mise.toml
-
-# --- SDKMAN (Java) ---
-# DISABLED: Now using mise for Java/Maven/Gradle/Scala/SBT
-# If you need SDKMAN, uncomment the sections below and remove mise Java tools
-
-# _sdkman_loaded=0
-#
-# _load_sdkman() {
-#   if [ $_sdkman_loaded -eq 1 ]; then
-#     return
-#   fi
-#   [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
-#   _sdkman_loaded=1
-# }
-#
-# sdk() {
-#   _load_sdkman
-#   command sdk "$@"
-# }
-#
-# java() {
-#   _load_sdkman
-#   unset -f java
-#   command java "$@"
-# }
-#
-# mvn() {
-#   _load_sdkman
-#   unset -f mvn
-#   command mvn "$@"
-# }
-#
-# gradle() {
-#   _load_sdkman
-#   unset -f gradle
-#   command gradle "$@"
-# }
-#
-# scala() {
-#   _load_sdkman
-#   unset -f scala
-#   command scala "$@"
-# }
-#
-# sbt() {
-#   _load_sdkman
-#   unset -f sbt
-#   command sbt "$@"
-# }
-#
-# _apply_sdkman_env() {
-#   if [[ -f .sdkmanrc ]]; then
-#     _load_sdkman
-#     sdk env
-#   fi
-# }
-
 # --- Zoxide ---
 cd() {
   if ! command -v __zoxide_z >/dev/null 2>&1; then
