560ea3c5f687ba7c4e9cd18fbbec0f0ede2e11b2
Chris Cho <chrischo360@gmail.com>
2025-12-27 15:28:59 -0500

    update kitty cursor coclor

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,15 @@
 require("config.lazy")
 
+-- Mode-specific cursor shapes for better visual feedback
+vim.opt.guicursor = {
+  "n-v-c:block",           -- Normal, Visual, Command: block cursor
+  "i-ci-ve:ver25",         -- Insert, Command Insert, Visual-Replace: thin vertical bar
+  "r-cr:hor20",            -- Replace, Command Replace: horizontal bar
+  "o:hor50",               -- Operator-pending: thick horizontal bar
+  "a:blinkwait700-blinkoff400-blinkon250",  -- All modes: controlled blinking
+  "sm:block-blinkwait175-blinkoff150-blinkon175",  -- Showmatch: fast blinking block
+}
+
 -- Git performance profiling command
 vim.api.nvim_create_user_command("ProfileGit", function()
   require("utils.profile-git").show_profile()
